c113acfb0e72f42262fd8951b8c0133f987f0aeb
Amir Liaqat <amirliaqat2020@gmail.com>
2022-06-17 10:44:36 +0500

    Adding new constants for easy to use...

--- bb-plugin.php
+++ bb-plugin.php
@@ -8,3 +8,19 @@
  * Version: 1.0.0
  * Licence: GPL v2 or Later
  */
+
+if(!defined('ABSPATH')) {
+    die();
+}
+
+if(!defined('PLUGIN_PATH')) {
+    define('PLUGIN_PATH', plugin_dir_path( __FILE__ ));
+}
+
+if(!defined('PLUGIN_URL')) {
+    define('PLUGIN_URL', plugin_dir_url( __FILE__ ));
+}
+
+if(!defined('PLUGIN')) {
+    define('PLUGIN', plugin_basename( __FILE__ ));
+}
