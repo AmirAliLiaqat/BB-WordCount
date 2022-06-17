550df2f0ec228041877d3cba6459f9a7d9ce1ea7
Amir Liaqat <amirliaqat2020@gmail.com>
2022-06-17 10:35:34 +0500

    Adding basic structure file in our plugin...

---
+++ bb-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/***
+ * Plugin Name: BB Plugin
+ * Plugin URI: https://www.bytebunch.com/plugins
+ * Author: ByteBunch
+ * Author URI: https://www.bytebunch.com
+ * Description: My first testing plugin for practice.
+ * Version: 1.0.0
+ * Licence: GPL v2 or Later
+ */
